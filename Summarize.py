1ef966a7a516254a599da769b8f8ddba5dbe2b80
Val Eltagonde <valeltagonde@gmail.com>
2023-09-03 14:36:54 +0800

    pytesseract fix

--- Summarize.py
+++ Summarize.py
@@ -6,7 +6,8 @@ import io
 from PIL import Image
 import pytesseract
 from PyPDF2 import PdfReader
-pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 
 # Import Langchain modules
 from langchain.chat_models import ChatOpenAI
